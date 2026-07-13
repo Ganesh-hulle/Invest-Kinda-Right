e5e9a8454baa1acc21fe43d2f1040c47b8e45fbd
Ganesh Hulle <hulleganesh07@gmail.com>
2026-07-13 23:26:15 +0530

    Initial Commit

---
+++ backend/src/main/java/com/ganesh/IKR/IkrApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.IKR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkrApplication.class, args);
+	}
+
+}

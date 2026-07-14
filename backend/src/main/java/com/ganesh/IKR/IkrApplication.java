58480ac3b712129529309705aa22a5b30cb680f4
Ganesh Hulle <hulleganesh07@gmail.com>
2026-07-14 22:04:48 +0530

    Add initial project configuration files and update application settings

--- backend/src/main/java/com/ganesh/IKR/IkrApplication.java
+++ backend/src/main/java/com/ganesh/IKR/IkrApplication.java
@@ -2,12 +2,12 @@ package com.ganesh.IKR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class IkrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IkrApplication.class, args);
 	}
-
 }
